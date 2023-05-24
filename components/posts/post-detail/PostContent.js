314ad4aa33941e920f61371257c32cc31e4bf56e
J.D. Browne <PeshwariNaan01@proton.me>
2023-05-24 15:29:36 +0200

    Added formatting parameters to markdown in PostContent. Added new blog post to demonstrate how to format code in the markdown.

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,14 +1,60 @@
+import Image from 'next/image';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import PostHeader from './PostHeader';
 import classes from './post-content.module.css';
 
 const PostContent = (props) => {
   const { post } = props;
+  //console.log('slug: ', post.slug);
+
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    //The image.alt is the text before the image location in the markdown file made available by react markdown.
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const _array = code.className.split('-');
+      const _language = _array[1];
+      return (
+        <SyntaxHighlighter style={atomDark} language={_language}>
+          {code.children[0]}
+        </SyntaxHighlighter>
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
