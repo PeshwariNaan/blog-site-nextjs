e617759a57993129e7f1f09bee7a0d794d38c4a1
J.D. Browne <PeshwariNaan01@proton.me>
2023-05-24 12:49:39 +0200

    Modified PostContent and added folder for content where I added a markdown file to test. Added lib folder to store future helper functions.

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -1,3 +1,4 @@
+import ReactMarkdown from 'react-markdown';
 import PostHeader from './PostHeader';
 import classes from './post-content.module.css';
 
@@ -14,7 +15,7 @@ const PostContent = () => {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
     </article>
   );
 };
---
+++ content/posts/getting-started-with-nextjs.md
@@ -0,0 +1,12 @@
+---
+title: 'Getting started with NextJS'
+date: '2023-10-15'
+image: getting-started-with-nextjs.png
+excerpt: NextJS is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR.
+isFeatured: true
+
+ ---
+
+ # Getting Started With NextJS
+
+ This is some regular text with a [link](https://google.com)
