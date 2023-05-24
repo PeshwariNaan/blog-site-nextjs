34e55a33799b4ba2496913c4518391e25d32b827
J.D. Browne <PeshwariNaan01@proton.me>
2023-05-24 14:20:28 +0200

    Modified PostContent and slug page and functions in the lib dir.

--- components/posts/post-detail/PostContent.js
+++ components/posts/post-detail/PostContent.js
@@ -2,20 +2,13 @@ import ReactMarkdown from 'react-markdown';
 import PostHeader from './PostHeader';
 import classes from './post-content.module.css';
 
-const DUMMY_POST = {
-  slug: 'getting-started-with-nextjs',
-  title: 'Getting Started with NextJS',
-  image: 'getting-started-nextjs.png',
-  content: '# This is a first post',
-  date: '2022-02-10',
-};
-
-const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+const PostContent = (props) => {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,6 +1,33 @@
 import PostContent from '@/components/posts/post-detail/PostContent';
+import { getContentData, getPostFiles } from '@/lib/post-util';
 
-const PostDetailPage = () => {
-  return <PostContent />;
+const PostDetailPage = (props) => {
+  return <PostContent post={props.post} />;
 };
 export default PostDetailPage;
+
+export const getStaticProps = (context) => {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getContentData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+};
+
+export const getStaticPaths = () => {
+  const postFileNames = getPostFiles();
+
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ''));
+
+  return {
+    //We are generating all possible paths for the posts in advance.
+    paths: slugs.map((slug) => ({ params: { slug: slug } })), //Syntax returns an immediate return object.
+    fallback: 'blocking',
+  };
+};
