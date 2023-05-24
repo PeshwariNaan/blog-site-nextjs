b33667b64510cf5c9e713e6d74d54977d9b370f0
J.D. Browne <PeshwariNaan01@proton.me>
2023-05-24 09:29:55 +0200

    Added styling and modified the Navigation component.

--- components/layout/navigation/Navigation.js
+++ components/layout/navigation/Navigation.js
@@ -1,4 +1,22 @@
+import Link from 'next/link';
+import Logo from '../logo/Logo';
+import classes from './main-navigation.module.css';
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  return (
+    <header className={classes.header}>
+      <Link href="/">
+        <Logo />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 export default Navigation;
