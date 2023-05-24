9f4893e9f99488d55b3b9e08705c183f1e20e6e7
J.D. Browne <PeshwariNaan01@proton.me>
2023-05-24 15:39:25 +0200

    Added ContactForm and styles.

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,9 @@
+import ContactForm from '@/components/contact/ContactForm';
+
 const ContactPage = () => {
   return (
     <div>
-      <h1>Contact page</h1>
+      <ContactForm />
     </div>
   );
 };
